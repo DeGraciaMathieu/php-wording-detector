16612424f11e4b4e3300a87060e0b52110c7aab0
DeGraciaMathieu <degracia.mathieu@gmail.com>
2023-03-06 08:17:00 +0100

    feat : add NodeValidatorTest

---
+++ tests/Unit/Nodes/NodeValidatorTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit\Nodes;
+
+use PHPUnit\Framework\TestCase;
+use App\Nodes\NodeValidator;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Expr\Array_;
+
+class NodeValidatorTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_able_to_check_node_is_a_variable(): void
+    {
+        $variable = new Variable(
+            name: 'getAllFoo',
+            attributes: [],
+        );
+
+        $isAVariable = NodeValidator::isAVariable($variable);
+
+        $this->assertTrue($isAVariable);
+    }
+
+    /**
+     * @test
+     */
+    public function it_able_to_handle_an_unexpected_node(): void
+    {
+        $variable = new Array_(
+            items: [],
+            attributes: [],
+        );
+
+        $isAVariable = NodeValidator::isAVariable($variable);
+
+        $this->assertFalse($isAVariable);
+    }
+
+}
